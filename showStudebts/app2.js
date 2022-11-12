bd110cb05e0a120ca04a9858c7f47735886d8b41
Shahmeer Hussain <97156017+Shahmeerhussain1@users.noreply.github.com>
2022-11-11 23:26:22 -0800

    updated

--- showStudebts/app2.js
+++ showStudebts/app2.js
@@ -1,3 +1,37 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import {
+  doc,
+  setDoc,
+  getFirestore,
+  addDoc,
+  collection,
+  getDocs 
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyDTgpzAlC6sszJY9uSrWOqzewgp86K6GLg",
+  authDomain: "data-64504.firebaseapp.com",
+  projectId: "data-64504",
+  storageBucket: "data-64504.appspot.com",
+  messagingSenderId: "870525670816",
+  appId: "1:870525670816:web:9eba97f0cd3c6601d380c5",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+
+
+
 
 
 
@@ -17,6 +51,27 @@ const attendence=()=>{
 
 
 
+let classesavailabletr = document.getElementById("classesavailabletr")
+const querySnapshot = await getDocs(collection(db, "Classes"));
+querySnapshot.forEach((doc) => {
+    classesavailabletr.innerHTML +=`
+    <tr>
+    <td>${doc.data().batchNumber}</td>
+    <td>${doc.data().classTiming}</td>
+    <td>${doc.data().courseName}</td>
+    <td>${doc.data().teacherName}</td>
+    <td>${doc.data().scetionName}</td>
+    <td><button onclick="edits('${event}')">Update</button></td>
+</tr>
+    `
+  console.log(doc.id, " => ", doc.data());
+});
+
+const edits=(event)=>{
+    console.log(event.target.parentNode )
+}
+
+window.edits=edits
 window.addstudents=addstudents
 window.createclass=createclass
 window.availablestudents=availablestudents
---
+++ showStudebts/createClass/createclass.js
@@ -0,0 +1,77 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import {
+  doc,
+  setDoc,
+  getFirestore,
+  addDoc,
+  collection,
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-firestore.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/9.13.0/firebase-storage.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyDTgpzAlC6sszJY9uSrWOqzewgp86K6GLg",
+  authDomain: "data-64504.firebaseapp.com",
+  projectId: "data-64504",
+  storageBucket: "data-64504.appspot.com",
+  messagingSenderId: "870525670816",
+  appId: "1:870525670816:web:9eba97f0cd3c6601d380c5",
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+let classTiming = document.getElementById("classTiming");
+let teacherName = document.getElementById("teacherName");
+let scetionName = document.getElementById("scetionName");
+let courseName = document.getElementById("courseName");
+let batchNumber = document.getElementById("batchNumber");
+let main_loader = document.getElementById("main_loader");
+let alllllmain = document.getElementById("alllllmain");
+
+let Createclassbtn = document.getElementById("Createclassbtn");
+Createclassbtn.addEventListener("click", async () => {
+  if (classTiming.value) {
+    if (teacherName.value) {
+      if (scetionName.value) {
+        if (courseName.value) {
+          if (batchNumber.value) {
+            alllllmain.style.display = "none";
+            main_loader.style.display = "block";
+            const docRef = await addDoc(collection(db, "Classes"), {
+              classTiming: classTiming.value,
+              teacherName: teacherName.value,
+              scetionName: scetionName.value,
+              courseName: courseName.value,
+              batchNumber: batchNumber.value,
+            });
+            classTiming.value = "";
+            teacherName.value = "";
+            scetionName.value = "";
+            courseName.value = "";
+            batchNumber.value = "";
+            main_loader.style.display = "none";
+            alllllmain.style.display = "block";
+          } else {
+            swal("Invalid inputs");
+          }
+        } else {
+          swal("Invalid inputs");
+        }
+      } else {
+        swal("Invalid inputs");
+      }
+    } else {
+      swal("Invalid inputs");
+    }
+  } else {
+    swal("Invalid inputs");
+  }
+});
